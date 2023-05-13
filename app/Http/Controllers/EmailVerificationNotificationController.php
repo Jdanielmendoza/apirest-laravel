0d1ea16b034400556c33802d60652c0f84bbdcc1
DanielMG <jdanielmendoza987@gmail.com>
2023-05-13 15:52:15 -0400

    si1

---
+++ app/Http/Controllers/EmailVerificationNotificationController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Responses\HasEmailResponse;
+use App\Http\Responses\SendEmailResponse;
+use Laravel\Fortify\Http\Controllers\EmailVerificationNotificationController as NewController;
+
+class EmailVerificationNotificationController extends NewController
+{
+    /**
+     * Send a new email verification notification.
+     * @param \Illuminate\Http\Request $request
+     * @return mixed
+     */
+    public function store(Request $request): SendEmailResponse
+    {
+        if($request->user()->hasVerifiedEmail()){
+            return app(HasEmailResponse::class);
+        }
+
+        $request->user()->sendEmailVerificationNotification();
+
+        return app(SendEmailResponse::class);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\RegisteredUserController;
+use Laravel\Fortify\Http\Controllers\PasswordResetLinkController;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
+use App\Http\Controllers\EmailVerificationNotificationController;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+$verificationLimiter = config('fortify.limiters.verification','6,1');
+Route::group(
+    [
+        'prefix' => config('app.apiversion'),
+        'as' => config('app.apiversion'),],
+        function(){
+            $limiter = config('fortify.limiters.login');
+            
+            Route::post('/login',[AuthenticatedSessionController::class,'store'])
+            ->middleware(array_filter([
+                'guest:'.config('fortify.guard'),
+                $limiter ? 'throttle'.$limiter : null,
+            ]));
+
+            Route::post('/register',[RegisteredUserController::class,'store'])
+
+            ->middleware(['guest:'.config('fortify.guard')]);
+
+            Route::post('/forgot-password',[PasswordResetLinkController::class,'store'])
+            ->middleware(['guest:'.config('fortify.guard')])
+            ->name('password.email');
+
+            
+        }
+);
+
+Route::group(
+    ['prefix' => config('app.apiversion'),
+    'as' => config('app.apiversio'),
+    'middleware' =>['auth:sanctum','throttle:'.$verificationLimiter]],
+    function (){
+        Route::post('/email/verification-notification',
+            [EmailVerificationNotificationController::class,'store'])
+            ->name('verification.send');
+    }
+);
+
+
+
